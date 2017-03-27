6463f992c729d755d6becae8c094b05309074a7f
Woulei <woulei@me.com>
2017-03-27 15:32:28 +0200

    Added the tests and spaces validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327122842) do
+ActiveRecord::Schema.define(version: 20170327125845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20170327122842) do
     t.string   "location"
     t.integer  "spaces"
     t.string   "image"
-    t.boolean  "public"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "public",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Check validations" do
+    context "title attribute" do
+      it "is invalid without it" do
+        event = Event.new(title: "")
+        event.valid?
+        expect(event.errors).to have_key(:title)
+      end
+      it "Is valid with it" do
+        event = Event.new(title: "An new title")
+        event.valid?
+        expect(event.errors).not_to have_key(:title)
+      end
+    end
+    context "location attribute" do
+      it "is invalid without it" do
+        event = Event.new(location: "")
+        event.valid?
+        expect(event.errors).to have_key(:location)
+      end
+      it "is valid with it" do
+        event = Event.new(location: "Lee's place")
+        event.valid?
+        expect(event.errors).not_to have_key(:location)
+      end
+    end
+    context "spaces attribute" do
+      it "Is invalid without it" do
+        event = Event.new(spaces: "")
+        event.valid?
+        expect(event.errors).to have_key(:spaces)
+      end
+      it "Is valid with it" do
+        event = Event.new(spaces: 25)
+        event.valid?
+        expect(event.errors).not_to have_key(:spaces)
+      end
+      it "is invalid if not an integer" do
+        event = Event.new(spaces: "text")
+        event.valid?
+        expect(event.errors).to have_key(:spaces)
+      end
+      it "is invalid with an decimal" do
+        event = Event.new(spaces: 0.5)
+        event.valid?
+        expect(event.errors).to have_key(:spaces)
+      end
+      it "is valid with an integer" do
+        event = Event.new(spaces: 5)
+        event.valid?
+        expect(event.errors).not_to have_key(:spaces)
+      end
+    end
+  end
 end
