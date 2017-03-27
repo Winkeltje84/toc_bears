fe562ca4534197252dfcd94dd265612f0dd66a9b
Woulei <woulei@me.com>
2017-03-27 15:04:13 +0200

    Typo in migration file

--- db/migrate/20170327125845_add_default_to_event_public.rb
+++ db/migrate/20170327125845_add_default_to_event_public.rb
@@ -1,5 +1,5 @@
 class AddDefaultToEventPublic < ActiveRecord::Migration[5.0]
   def change
-    change_column_default :event, :public, false
+    change_column_default(:events, :public, false)
   end
 end
