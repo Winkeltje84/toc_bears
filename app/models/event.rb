46ef2010a9bcfddc3be246b36d0fc98a651c2e94
Woulei <woulei@me.com>
2017-03-27 15:00:51 +0200

    Added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  validates :title, presence:true
+  validates :location, presence:true
+  validates :spaces, presence:true
+
+  #will need to add the workshop and sponsor relations
 end
