f1be43bec703ee367f344d55fac2684faf68406f
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-03-28 17:16:02 +0200

    Created 3 Panel of Events

--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,4 @@ Rails.application.routes.draw do
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: 'pages#home'
-
-
-    resourses :sponsors
-    resources :events do
-                          companies
-                          workshops do users
-                          end
-                          end
-    "event/book", to: "event#book"
-    "event/sponsor" to: "sponsor#sponsor"
-    "event/workshop/attend" to: "workshop/attend"
-
-
-
 end
