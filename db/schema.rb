12f02544849da301ee1944f107ac0bf55781b1c8
Woulei <woulei@me.com>
2017-03-29 15:32:41 +0200

    Added the date column to the events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327131238) do
+ActiveRecord::Schema.define(version: 20170329132752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170327131238) do
     t.boolean  "public",     default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.datetime "date"
   end
 
   create_table "sponsors", force: :cascade do |t|
