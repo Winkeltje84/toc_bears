c1e1197738e564ab1c7d7a2ca6113f403d5549db
anikaSchwing <anikaschwing@gmail.com>
2017-03-29 10:51:42 +0200

    created view test

---
+++ spec/views/pages/calculate_costs_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "shared/calculate_costs.html.erb", type: :view do
+
+  it "contains Costs Calculation text" do
+    render :partial => 'shared/calculate_costs.html.erb'
+    expect(rendered).to have_content("Costs Calculation")
+  end
+
+end
