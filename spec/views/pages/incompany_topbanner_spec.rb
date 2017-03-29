bca50539b03ddee5508f377b768e0c8eacbcf884
anikaSchwing <anikaschwing@gmail.com>
2017-03-29 15:28:52 +0200

    removed hasrocket

--- spec/views/pages/incompany_topbanner_spec.rb
+++ spec/views/pages/incompany_topbanner_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "shared/incompany_topbanner.html.erb", type: :view do
 
   it "contains Taste of Code text" do
-    render :partial => 'shared/incompany_topbanner.html.erb'
+    render 'shared/incompany_topbanner.html.erb'
     expect(rendered).to have_content("Taste of Code")
   end
 
