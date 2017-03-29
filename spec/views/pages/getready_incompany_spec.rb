9a353c11a2aea0fbeb57d4783336448c23f96d2d
Winkeltje84 <bwinkel@gmail.com>
2017-03-29 13:37:23 +0200

    added spec that is not failing

---
+++ spec/views/pages/getready_incompany_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "shared/getready_incompany.html.erb", type: :view do
+
+  it "contains getready_incompany text" do
+    render :partial => 'shared/getready_incompany.html.erb'
+    expect(rendered).to have_content("Get Ready for the Day")
+    expect(rendered).to have_content("Requirements")
+  end
+
+end
