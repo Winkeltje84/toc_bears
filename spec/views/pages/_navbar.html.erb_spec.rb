5b7cbcc1112c5e7fcaab44cdf1a423f7a3059849
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-29 11:41:24 +0200

    added two tests which caught a typo

--- spec/views/pages/_navbar.html.erb_spec.rb
+++ spec/views/pages/_navbar.html.erb_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "shared/_navbar.html.erb", type: :view do
   describe "view tests" do
-    it "contains the Join Taste of Code test" do
+    it "contains contains the text join taste of code" do
       render :partial => 'shared/navbar.html.erb'
       expect(rendered).to have_content("Join Taste of Code")
+    end
+    it "contains contains the text in company" do
+      render :partial => 'shared/navbar.html.erb'
+      expect(rendered).to have_content("In Company")
+    end
+    it "contains contains the text host taste of code" do
+      render :partial => 'shared/navbar.html.erb'
+      expect(rendered).to have_content("Host Taste of Code")
     end
   end
 
