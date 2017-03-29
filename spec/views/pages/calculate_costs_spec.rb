90fa5307b92d0add38a8d1c907de1fb6bfa8b48a
anikaSchwing <anikaschwing@gmail.com>
2017-03-29 15:23:52 +0200

    removed hashrocket

--- spec/views/pages/calculate_costs_spec.rb
+++ spec/views/pages/calculate_costs_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "shared/calculate_costs.html.erb", type: :view do
 
   it "contains Costs Calculation text" do
-    render :partial => 'shared/calculate_costs.html.erb'
+    render 'shared/calculate_costs.html.erb'
     expect(rendered).to have_content("Costs Calculation")
   end
 
