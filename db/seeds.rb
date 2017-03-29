d4c0b4024adcaefd152c34c8f746caf194a3744c
Woulei <woulei@me.com>
2017-03-29 15:50:36 +0200

    Small edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 User.delete_all
+puts "Deleted all the users"
 
-User.create!(email: "wouter@codaisseur.com", password: "testing")
-User.create!(email: "valentina@codaisseur.com", password: "testing")
+User.create!(email: "wouter@codaisseur.com", password: "testing", admin: true)
+User.create!(email: "valentina@codaisseur.com", password: "testing", admin: true)
+puts "Created new users"
