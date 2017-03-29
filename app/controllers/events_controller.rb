15346025004faa6bd9b662821d41a3ea1d996758
anikaSchwing <anikaschwing@gmail.com>
2017-03-29 17:00:29 +0200

    Created events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update]
+  before_action :authenticate_user!
+
+  def index
+    @events = Event.all
+  end
+
+  def show; end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event created"
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @event = Event.update(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event updated"
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:title, :location, :spaces, :image, :public, :date)
+    end
+end
