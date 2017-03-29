545697d16d8a93508946df7bf171d23fedd8c40e
Winkeltje84 <bwinkel@gmail.com>
2017-03-29 15:09:42 +0200

    added spec that fails if texts are changed

---
+++ spec/views/pages/newsletter_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "shared/newsletter.html.erb", type: :view do
+
+  it "contains newsletter text" do
+    render :partial => 'shared/newsletter.html.erb'
+    expect(rendered).to have_content("Join our newsletter")
+    expect(rendered).to have_content("Be the first one")
+    expect(rendered).not_to have_content("TEXT NOT ON PAGE")
+  end
+
+end
