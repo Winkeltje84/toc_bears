8da5c00291f212776f0ada3d6c43afede59a15ae
Winkeltje84 <bwinkel@gmail.com>
2017-03-29 16:12:44 +0200

    replaced spec file

--- spec/views/shared/getready_incompany_spec.rb
+++ spec/views/shared/getready_incompany_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "shared/getready_incompany.html.erb", type: :view do
 
   it "contains getready_incompany text" do
-    render :partial => 'shared/getready_incompany.html.erb'
+    render partial: 'shared/getready_incompany.html.erb'
     expect(rendered).to have_content("Get Ready for the Day")
     expect(rendered).to have_content("Requirements")
   end
