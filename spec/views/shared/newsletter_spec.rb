5045f82e3866499e99d87e01a61c65805d4ecc9f
Winkeltje84 <bwinkel@gmail.com>
2017-03-29 16:55:00 +0200

    replaced spec & deleted empty line

--- spec/views/shared/newsletter_spec.rb
+++ spec/views/shared/newsletter_spec.rb
@@ -6,7 +6,5 @@ RSpec.describe "shared/newsletter.html.erb", type: :view do
     render partial: 'shared/newsletter.html.erb'
     expect(rendered).to have_content("Join our newsletter")
     expect(rendered).to have_content("Be the first one")
-    expect(rendered).not_to have_content("TEXT NOT ON PAGE")
   end
-
 end
