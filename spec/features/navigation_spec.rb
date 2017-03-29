91cc77afdda27dd4ba8e792fabb789738944261a
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-29 17:00:36 +0200

    indentation

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'navigation', type: :feature do
-    describe 'testing links' do
-        it 'goes to the public page when the join taste of code link is clicked' do
-          visit attend_path
-          click_link('Join Taste of Code')
-          expect(current_path).to eq(attend_path)
-        end
-
-        it 'goes to the public page when the join taste of code link is clicked' do
-          visit attend_path
-          click_link('In Company')
-          expect(current_path).to eq(incompany_path)
-        end
-        
-        it 'goes to the public page when the join taste of code link is clicked' do
-          visit attend_path
-          click_link('Host Taste of Code')
-          expect(current_path).to eq(sponsor_path)
-        end
+  describe 'testing links' do
+    it 'goes to the public page when the join taste of code link is clicked' do
+      visit attend_path
+      click_link('Join Taste of Code')
+      expect(current_path).to eq(attend_path)
     end
+
+    it 'goes to the public page when the join taste of code link is clicked' do
+      visit attend_path
+      click_link('In Company')
+      expect(current_path).to eq(incompany_path)
+    end
+
+    it 'goes to the public page when the join taste of code link is clicked' do
+      visit attend_path
+      click_link('Host Taste of Code')
+      expect(current_path).to eq(sponsor_path)
+    end
+  end
 end
