cea3b7bf05513504c7414bb378a19cc7ae1b59a2
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-30 15:48:15 +0200

    working controller tests

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ end
 
 group :test do
   gem 'capybara', '~> 2.9', '>= 2.9.1'
+  gem 'rails-controller-testing'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/controllers/workshops_controller_spec.rb
+++ spec/controllers/workshops_controller_spec.rb
@@ -1,6 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe WorkshopsController, type: :controller do
-  #to do
-
+  let!(:workshop) { create :workshop }
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'GET #new' do
+    it 'returns http success' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'GET #show' do
+    it 'returns http success' do
+      get :show, params: { id: workshop.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'GET #edit' do
+    it 'returns http success' do
+      get :edit, params: { id: workshop.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'POST #create' do
+    it 'returns http success' do
+      post :create, workshop: FactoryGirl.attributes_for(:workshop)
+      assert_redirected_to workshop_path(assigns(:workshop).id)
+    end
+  end
+  describe 'POST #create' do
+    it 'redirects to the new workshop' do
+      post :create, workshop: FactoryGirl.attributes_for(:workshop)
+      assert_redirected_to workshop_path(assigns(:workshop).id)
+    end
+  end
+  describe 'PATCH/PUT #update' do
+    it 'redirects to the workshop path' do
+      patch :update, params: { id: workshop.id, workshop: workshop.attributes = { title: 'Three bears eating porridge on a cold day' } }
+      assert_redirected_to workshop_path(workshop.id)
+    end
+  end
+  describe 'DELETE #destroy' do
+    it 'redirects to the workshops path' do
+      delete :destroy, params: { id: workshop.id }
+      assert_redirected_to workshops_path
+    end
+  end
 end
