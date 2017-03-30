2f6585a6f02ca18e386bfc97634262a8cadbdbca
Woulei <woulei@me.com>
2017-03-30 16:05:15 +0200

    final changes

--- spec/features/events/index_html_erb_spec.rb
+++ spec/features/events/index_html_erb_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'events/index', type: :feature do
   before { login_as user }
   let(:user) { create :user, admin: true}
+  let!(:public_event) { create :event, title: "This event is public", public: true }
+  let!(:incompany_event) { create :event, title: "This event is not public", public: false }
 
   context "renders partials" do
     it "renders public events partial" do
@@ -15,8 +17,6 @@ RSpec.describe 'events/index', type: :feature do
     end
   end
   context "displays events" do
-    let!(:public_event) { create :event, title: "This event is public", public: true }
-    let!(:incompany_event) { create :event, title: "This event is not public", public: false }
     it "shows the event information" do
       visit events_path
       expect(page).to have_text("This event is public")
