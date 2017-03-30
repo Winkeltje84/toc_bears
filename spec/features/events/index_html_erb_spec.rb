d1f44c89c09b684f4f09fe37e0d401d0b8b7f922
Woulei <woulei@me.com>
2017-03-30 18:31:40 +0200

    Fix test, less but better tests

--- spec/features/events/index_html_erb_spec.rb
+++ spec/features/events/index_html_erb_spec.rb
@@ -6,17 +6,7 @@ RSpec.describe 'events/index', type: :feature do
   let!(:public_event) { create :event, title: "This event is public", public: true }
   let!(:incompany_event) { create :event, title: "This event is not public", public: false }
 
-  context "renders partials" do
-    it "renders public events partial" do
-      visit events_path
-      expect(page).to have_text("Public Events")
-    end
-    it "renders incompany partial" do
-      visit events_path
-      expect(page).to have_text("Incompany Events")
-    end
-  end
-  context "displays events" do
+  context "displays events table partial" do
     it "shows the event information" do
       visit events_path
       expect(page).to have_text("This event is public")
@@ -26,5 +16,10 @@ RSpec.describe 'events/index', type: :feature do
       expect(page).to have_text("This event is public")
       expect(page).to have_text("This event is not public")
     end
+  end
+  it "links to the right event" do
+    visit events_path
+    click_link public_event.title
+    expect(current_path).to eq(event_path(public_event))
   end
 end
