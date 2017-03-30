a44bf64293f49259539759c5dbca6ee46fa9d620
Winkeltje84 <bwinkel@gmail.com>
2017-03-30 12:49:09 +0200

    working on CRUD... only the Destroy to do still

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,2 +1,46 @@
 class SponsorsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @sponsor = Sponsor.new
+  end
+
+  def index
+    @sponsors = Sponsors.all
+  end
+
+  def create
+    @sponsor = Sponsor.create(sponsor_params)
+
+    if @sponsor.save
+      redirect_to @sponsor, notice: "Sponsor created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @sponsor = Sponsor.update(sponsor_params)
+
+    if @sponsor.save
+      redirect_to @sponsor, notice: "Sponsor updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
 end
+
+private
+  def set_sponsor
+    @sponsor = Sponsor.find(params[:id])
+  end
+
+  def sponsor_params
+    params.require(:sponsor).permit(:company_name, :logo)
+  end
