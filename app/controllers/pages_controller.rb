770ff23e8f1b8c15c219dae8542bde478e3f0e76
Caroline Olijve <olijvecaroline@gmail.com>
2017-03-31 11:44:45 +0200

    form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   def homepage;end
   def attend;end
   def incompany;end
-  def sponsor;end
+  def sponsor
+@sponsor_request = Sponsorrequest.new
+  end
 end
