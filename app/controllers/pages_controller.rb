cecfd9ef0f6b087c1343d9a2b2548e0f066acd64
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-31 13:57:14 +0200

    upcoming events partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
 class PagesController < ApplicationController
   def homepage;end
-
   def attend
     @sponsors = Sponsor.all
+    @events = Event.all
   end
-
   def incompany;end
   def sponsor
     @sponsor_request = SponsorRequest.new
